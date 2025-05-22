789a43d947fbc842fb8c4138e8d10d78e87752b6
Olga <xotaoshi@gmail.com>
2025-05-22 20:44:19 +0300

    Add project files.

---
+++ Classwork/unique_ptr.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <iostream>
+
+template <typename T>
+class MyUniquePtr {
+private:
+    T* ptr_;
+    Del deleter_;
+
+public:
+	MyUniquePtr();
+	MyUniquePtr(T* ptr = nullptr) : ptr_(ptr) {}
+	MyUniquePtr(nullptr_t Nptr);
+	explicit MyUniquePtr(const MyUniquePtr&) = delete;
+
+
+};
+
